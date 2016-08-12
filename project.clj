c2365e89c184e14ffe11c6410d3e83c0ae9ec411
Markus Hjort <markus.hjort@gmail.com>
2016-08-12 22:23:43 +0300

    Add local queue reduce

--- project.clj
+++ project.clj
@@ -6,7 +6,9 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [cheshire "5.5.0"]
                  [serializable-fn "1.1.4"]
+                 [org.clojure/core.async "0.2.374"]
                  [com.amazonaws/aws-java-sdk-lambda "1.10.50"]
+                 [com.amazonaws/aws-java-sdk-sqs "1.10.50"]
                  [com.amazonaws/aws-java-sdk-core "1.10.50"]
                  [uswitch/lambada "0.1.2"]]
   :lambda {"demo" [{:handler "distributed-transducers-poc.LambdaFn"
--- src/distributed_transducers_poc/rc.clj
+++ src/distributed_transducers_poc/rc.clj
@@ -1,10 +1,13 @@
 (ns distributed-transducers-poc.rc
   (:require [cheshire.core :refer [generate-string parse-string parse-stream]]
+            [clojure.core.async :refer [go]]
             [clojure.java.io :as io]
             [serializable.fn :as s])
   (:import [com.amazonaws ClientConfiguration]
            [com.amazonaws.auth DefaultAWSCredentialsProviderChain]
            [com.amazonaws.regions Regions]
+           [com.amazonaws.services.sqs AmazonSQSClient]
+           [com.amazonaws.services.sqs.model ReceiveMessageRequest]
            [com.amazonaws.services.lambda.model InvokeRequest]
            [com.amazonaws.services.lambda AWSLambdaClient]))
 
@@ -33,7 +36,67 @@
 (defmacro super-reduce [f xs]
   `(invoke-lambda (pr-str (s/fn [] (reduce ~f ~xs))) "distributed-transducers-poc" "eu-west-1"))
 
-;(super-reduce + (range 100000))
+(defn plus [a b]
+  (+ a b))
+
+(def sqs-client (AmazonSQSClient. aws-credentials))
+
+(defn create-queue [queue-name]
+  (let [queue-url (-> sqs-client
+                      (.createQueue queue-name)
+                      (.getQueueUrl))]
+    (loop []
+      (let [urls (.getQueueUrls (.listQueues sqs-client queue-name))]
+      (when (empty? urls)
+        (Thread/sleep 200)
+        (recur))))
+    queue-url))
+
+(defn send-message [queue-url payload]
+  (.sendMessage sqs-client
+                queue-url
+                (generate-string payload)))
+
+(defn receive-message [queue-url wait-in-seconds]
+  (let [to-json #(parse-string % true)]
+    (-> (.receiveMessage sqs-client (-> (ReceiveMessageRequest.)
+                                        (.withQueueUrl queue-url)
+                                        (.withWaitTimeSeconds (int wait-in-seconds))))
+        (.getMessages)
+        (first)
+        (.getBody)
+        (to-json))))
+
+(defn delete-queue [queue-url]
+  (.deleteQueue sqs-client queue-url))
+
+(defn message-handler [f in-queue out-queue]
+  (go
+    (let [payload (receive-message in-queue 5)
+          response {:result (f (:param payload))}]
+      (send-message out-queue response))))
+
+(defn uuid [] (str (java.util.UUID/randomUUID)))
+
+(defn queue-reduce [f xs in-queue out-queue]
+  (let [batches (partition-all 1024 xs)]
+    (reduce (fn [acc batch]
+              (send-message in-queue {:param batch})
+              (message-handler #(reduce f %) in-queue out-queue)
+              (let [response (receive-message out-queue 5)]
+                (f acc (:result response))))
+            0
+            batches)))
+
+(comment
+(let [in (create-queue (uuid))
+      out (create-queue (uuid))]
+  (let [response (queue-reduce + (range 10000) in out)]
+    (delete-queue in)
+    (delete-queue out)
+    response))
+)
+  ;(super-reduce plus (range 100000))
 
 ;(invoke-lambda (pr-str (s/fn [] (map #(* 2 %) [3 4 5]))) "distributed-transducers-poc" "eu-west-1")
 
